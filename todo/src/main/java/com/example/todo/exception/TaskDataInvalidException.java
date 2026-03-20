9cea142e61b5b691e50ded1e0ef13c395d6515f3
HananMurrar <121627103+HananMurrar@users.noreply.github.com>
2026-03-20 19:08:46 +0200

    Delete todo directory

--- todo/src/main/java/com/example/todo/exception/TaskDataInvalidException.java
+++
@@ -1,8 +0,0 @@
-package com.example.todo.exception;
-
-public class TaskDataInvalidException extends RuntimeException {
-    public TaskDataInvalidException(String message) {
-        // passes a friendly error message to RuntimeException
-        super(message);
-    }
-}
